ddd3f6ee97bce78367ef593c05591b63e3572c7a
JhoanESG <94477658+JhoanESG@users.noreply.github.com>
2022-03-25 19:54:34 -0500

    Inicio del proyecto. version 1.0

---
+++ src/co/edu/uniquindio/proyectoCine/Main.java
@@ -0,0 +1,10 @@
+package co.edu.uniquindio.proyectoCine;
+
+import javax.swing.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/co/edu/uniquindio/proyectoCine/cine/Persona.java
@@ -0,0 +1,28 @@
+package co.edu.uniquindio.proyectoCine.cine;
+
+public class Persona {
+
+    private String nombre;
+    private String cedula;
+    private boolean sexo;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public boolean isSexo() {
+        return sexo;
+    }
+
+    public void setSexo(boolean sexo) {
+        this.sexo = sexo;
+    }
+}
